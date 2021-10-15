68db36837e2f61fcf75318b0dab0337ee2915f1d
bramerto-cmd <64208856+bramerto-cmd@users.noreply.github.com>
2021-10-16 00:07:47 +0300

    s

--- rateproject/main.py
+++ rateproject/main.py
@@ -19,7 +19,7 @@ while True:
     res = c.get_rate("USD", "RUB")
     val = round(res)
     try:
-        vk.wall.edit(post_id=post_id, message=f"Текущий курс доллара: {val} рублей\nОбновление курса происходит автоматически каждые 24 часа\nПодробнее здесь: https://github.com/svelout/exchangeratevk")
+        vk.wall.edit(post_id=post_id, message=f"Текущий курс доллара: {val} руб.\nОбновление курса происходит автоматически каждые 24 часа\nПодробнее здесь: https://github.com/svelout/exchangeratevk")
     except Exception as e:
         vk.messages.send(message=f"Произошла ошибка: {e}")
         sys.exit(-1)
