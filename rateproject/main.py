1c968f82efd688eec1742c28da01a820ff8792f7
bramerto-cmd <64208856+bramerto-cmd@users.noreply.github.com>
2021-10-15 21:47:40 +0300

    s

--- rateproject/main.py
+++ rateproject/main.py
@@ -1,6 +1,8 @@
 from forex_python.converter import CurrencyRates as rate
-import time
+import pytz
 import sys
+import time
+from datetime import datetime
 from vk_api import VkApi
 login = str(input("Login: "))
 password = str(input("Password: "))
@@ -12,15 +14,21 @@ except Exception as e:
     print(e)
     sys.exit(1)
 post_id = int(input("PostId: "))
+print("Good! We starting work")
 vk = vk_session.get_api()
 c = rate()
 while True:
-    res = c.get_rate("USD", "RUB")
-    val = round(res, 1)
-    try:
-     vk.wall.edit(post_id=post_id, message=f"Текущий курс доллара: {val} рублей\nОбновление курса происходит автоматически каждые 24 часа\nПодробнее здесь: https://github.com/svelout/exchangeratevk")
-    except Exception as e:
-        vk.messages.send(message=f"Произошла ошибка: {e}")
+    now = datetime.now()
+    if now.hour == 00:
+        res = c.get_rate("USD", "RUB")
+        val = round(res, 1)
+        try:
+           vk.wall.edit(post_id=post_id, message=f"Текущий курс доллара: {val} рублей\nОбновление курса происходит автоматически каждые 24 часа\nПодробнее здесь: https://github.com/svelout/exchangeratevk")
+        except Exception as e:
+           vk.messages.send(message=f"Произошла ошибка: {e}")
         sys.exit(-1)
-    time.sleep(86400)
+    else:
+        print("Continue waiting...")
+        time.sleep(5)
+
 
---
+++ rateproject/tets.py
@@ -0,0 +1,4 @@
+from datetime import datetime
+
+d = datetime.now()
+print(d.hour)
